114a49ff6069880fd10a117cc8f6f5caae6dfd1b
Duygu Temur <duygutemur@archis.company>
2020-12-08 21:35:22 +0300

    find total price

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,126 +3,134 @@ import './App.css'
 import './ProductCard.css'
 import { setInLocal, getInLocal } from '../utils/localStorage'
 
-  class ProductCard extends React.Component {
-    state = {
-        isClicked: true,
-        totalPrice : this.props.product.price
-      }
-
-      componentDidMount() {
-        this.setState({isClicked: getInLocal('addedProducts') ? 
-          !getInLocal('addedProducts').includes(this.props.product.id.toString()) : 
-          true})
-      }
-    
-      addToCart = () => {
-        this.setState({isClicked: !this.state.isClicked})
-        let existing = getInLocal('addedProducts')
-        existing = existing ? existing.split(',') : [];
-        existing.push(this.props.product.id);
-        setInLocal('addedProducts', existing.toString())
-        if(!this.props.rerenderParentCallback) {
-          this.props.incrementCount();
-        }
-        
-        if(this.props.rerenderParentCallback) {
-          this.props.rerenderParentCallback();
-        }
-      }
-
-      removeFromCart = () => {
-        this.setState({isClicked: !this.state.isClicked})
-        let existing = getInLocal('addedProducts')
-        const arr = existing.split(',')
-        const index = arr.indexOf(this.props.product.id.toString());
-        if (index > -1) {
-          arr.splice(index, 1);
-        }
-        setInLocal('addedProducts', arr.toString())
-        if(!this.props.rerenderParentCallback) {
-          this.props.decrementCount();
-        }
-        
-        if(this.props.rerenderParentCallback) {
-          this.props.rerenderParentCallback();
-        }
-      }
-
-
-      onChange = (event) => {
-        this.setState({
-          totalPrice: event.target.value * this.props.product.price
-        })
-      }
-
-
-     
-    render() {
-        return (
-          <div className="ui container">
-            <div className="ui cards">
-                <div className="card">
-                  <img className="image" src={this.props.product.image} alt="product" />
-                  <div className="products-details">
-                    <h1>{this.props.product.title}</h1>
-                    <p>{this.props.product.description}</p>
-
-                    {this.props.showTable ? 
-                    (<table className="ui fixed single line celled table">
-                    <tbody>
-                      <tr>
-                        <td>Seri</td>
-                        <td>Laptop 15-da2088nt</td>
-                      </tr>
-                      <tr>
-                        <td>Ekran</td>
-                        <td>15.6</td>
-                      </tr>
-                      <tr>
-                        <td>Ram Boyutu</td>
-                        <td>512</td>
-                      </tr>
-                      <tr>
-                        <td>İşlemci Sayısı</td>
-                        <td>4</td>
-                      </tr>
-                      <tr>
-                        <td>İşetim Sistemi</td>
-                        <td>FreeDOS</td>
-                      </tr>
-                    </tbody>
-                  </table>) : 
-                  
-                  <div className="total">
-                  <p>Miktar:</p>
-                  <select className="amount" onChange={this.onChange} value={this.state.value}>  
-                  <option name="amount" value="1">1</option>
-                  <option name="amount" value="2">2</option>
-                  <option name="amount" value="3">3</option>
-                  <option name="amount" value="4">4</option>
-                  <option name="amount" value="5">5</option>
-                  </select>
-                  </div>
-                  }
-
-              <h3>{this.state.totalPrice} TL</h3>
-             
-    
-    
-                    {this.state.isClicked ?
-                      <button className="ui button" onClick={this.addToCart}> Sepete Ekle </button>
-                      :
-                      <button className="ui button" onClick={this.removeFromCart} >Sepetten Çıkar</button>
-                    }
-                  </div>
-    
-                </div>
-            </div>
-          </div>
-        );
-      }
+class ProductCard extends React.Component {
+  state = {
+    isClicked: true,
+    cardPrice: this.props.product.price
   }
 
-  export default ProductCard
+  componentDidMount() {
+    const { props } = this;
+    this.setState({
+      isClicked: getInLocal('addedProducts') ?
+        !getInLocal('addedProducts').includes(props.product.id.toString()) :
+        true
+    })
+
+    if (this.props.rerenderParentCallback) {
+      props.updateProductTotal({id: props.product.id, amount: props.product.price});
+    }
+  }
+
+  addToCart = () => {
+    this.setState({ isClicked: !this.state.isClicked })
+    let existing = getInLocal('addedProducts')
+    existing = existing ? existing.split(',') : [];
+    existing.push(this.props.product.id);
+    setInLocal('addedProducts', existing.toString())
+    if (!this.props.rerenderParentCallback) {
+      this.props.incrementCount();
+    }
+
+    if (this.props.rerenderParentCallback) {
+      this.props.rerenderParentCallback();
+    }
+  }
+
+  removeFromCart = () => {
+    this.setState({ isClicked: !this.state.isClicked })
+    let existing = getInLocal('addedProducts')
+    const arr = existing.split(',')
+    const index = arr.indexOf(this.props.product.id.toString());
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    setInLocal('addedProducts', arr.toString())
+    if (!this.props.rerenderParentCallback) {
+      this.props.decrementCount();
+    }
+
+    if (this.props.rerenderParentCallback) {
+      this.props.rerenderParentCallback();
+    }
+  }
+  onChange = (event) => {
+    const { props } = this;
+    const total = event.target.value * props.product.price;
+    this.setState({
+      cardPrice: total
+    })
+
+    if (this.props.rerenderParentCallback) {
+      this.props.updateProductTotal({id: props.product.id, amount: total});
+    }
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div className="ui cards">
+          <div className="card">
+            <img className="image" src={this.props.product.image} alt="product" />
+            <div className="products-details">
+              <h1>{this.props.product.title}</h1>
+              <p>{this.props.product.description}</p>
+
+              {this.props.showTable ?
+                (<table className="ui fixed single line celled table">
+                  <tbody>
+                    <tr>
+                      <td>Seri</td>
+                      <td>Laptop 15-da2088nt</td>
+                    </tr>
+                    <tr>
+                      <td>Ekran</td>
+                      <td>15.6</td>
+                    </tr>
+                    <tr>
+                      <td>Ram Boyutu</td>
+                      <td>512</td>
+                    </tr>
+                    <tr>
+                      <td>İşlemci Sayısı</td>
+                      <td>4</td>
+                    </tr>
+                    <tr>
+                      <td>İşetim Sistemi</td>
+                      <td>FreeDOS</td>
+                    </tr>
+                  </tbody>
+                </table>) :
+                <div className="total">
+                  <p>Miktar:</p>
+                  <select className="amount" onChange={this.onChange} value={this.state.value}>
+                    <option name="amount" value="1">1</option>
+                    <option name="amount" value="2">2</option>
+                    <option name="amount" value="3">3</option>
+                    <option name="amount" value="4">4</option>
+                    <option name="amount" value="5">5</option>
+                  </select>
+                </div>
+              }
+
+              <h3>{this.state.cardPrice} TL</h3>
+
+
+
+              {this.state.isClicked ?
+                <button className="ui button" onClick={this.addToCart}> Sepete Ekle </button>
+                :
+                <button className="ui button" onClick={this.removeFromCart} >Sepetten Çıkar</button>
+              }
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductCard
 
 
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -5,12 +5,13 @@ import ProductCard from './ProductCard'
 import { getInLocal } from '../utils/localStorage'
 import products from '../products'
 import Button from './Button'
-import EmptyCart from './EmptyCart';
-import Price from './Price'
+import EmptyCart from './EmptyCart'
+
 
 class ShoppingCart extends React.Component {
     state = {
-        addedProducts: []
+        addedProducts: [],
+        productTotal: []
     }
 
     componentDidMount() {
@@ -26,29 +27,49 @@ class ShoppingCart extends React.Component {
         })
     }
 
+    updateProductTotal = (obj) => {
+        const productIdFound = this.state.productTotal.find(product => obj.id === product.id);
+
+        if(productIdFound) {
+            
+            this.setState({
+                productTotal: this.state.productTotal.map(product => (product.id === obj.id ? {...product, amount: obj.amount} : product))
+            });
+
+            return;
+        }
+        
+
+        this.setState(prevState => ({
+            productTotal: [...prevState.productTotal, obj]
+        }))
+
+    }
+
+
     rerenderParentCallback = () => {
         this.setState({ addedProducts: this.getSavedProducts() || [] })
     }
 
-  
-
 
     render() {
+        console.log(this.state.productTotal);
         return (
             <div className="ui container">
                 {this.state.addedProducts.length > 0 ?
-                <div className="page-head">
-                    <h1 className="page-title">Sepetteki Ürünler</h1>
-                </div>
-                : <EmptyCart/>
-    }
+                    <div className="page-head">
+                        <h1 className="page-title">Sepetteki Ürünler</h1>
+                    </div>
+                    : <EmptyCart />
+                }
                 <div className="ui cards">
-                    {this.state.addedProducts.map((product) => 
-                    <ProductCard key={product.id} product={product} showTable={false} rerenderParentCallback={this.rerenderParentCallback} />)}
-                    <Price />
-                   
+                    {this.state.addedProducts.map((product) =>
+                        <ProductCard key={product.id} product={product} showTable={false} rerenderParentCallback={this.rerenderParentCallback} updateProductTotal={this.updateProductTotal} />)}
+                    <h2>
+                        Toplam: {this.state.productTotal.reduce((a, b) => +a + b.amount, 0)}
+                    </h2>
                     {this.state.addedProducts.length > 0 ? <Button /> : ''}
-                   
+
                 </div>
             </div>
         )
